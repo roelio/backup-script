9e85f4620d478f2f8a5789fcb2fd260c85183e52
thebestsolution <roel@thebestsolution.org>
2012-11-13 17:39:28 +0100

    Added optional backing up of mysql by entering mysql user and password

--- backup.sh
+++ backup.sh
@@ -1,34 +1,62 @@
 #!/bin/bash
 
-if [ -n $1 ]; then
+#Backup name
+if [ -n "$1" ]; then
     BACKUP_NAME=$1  # Name of backup (log file, dest dir, exclude)
 else 
     exit 1
 fi
+
+# Optional hostname
 if [ -n "$2" ]; then
-    SRC=$2          # Source files (including host)
+    HOST=$2          # host
+else 
+    HOST=""
+fi
+
+# Source files
+if [ -n "$3" ]; then 
+    if [ -n "$HOST" ]; then 
+        SRC=${HOST}:${SRC}
+    else 
+        SRC=$3         
+    fi    
 else 
     exit 1
 fi
-if [ -n "$3" ]; then
-    BACKUP_REPO=$3  # Repository location
-else 
-    exit 1
-fi
+
+# Repository location
 if [ -n "$4" ]; then
-    SSH_COMMAND=$4  # Custom ssh command (like ssh -i keyfile -l remote_user)
+    BACKUP_REPO=$4  
+else 
+    exit 1
+fi
+
+# Custom ssh command (like ssh -i keyfile -l remote_user)
+if [ -n "$5" ]; then
+    SSH_COMMAND=$5  
 else 
     SSH_COMMAND=""
 fi
-if [ -n "$5" ]; then
-    EMAIL=$5        # Email for logging
+
+# Email for logging
+if [ -n "$6" ]; then
+    EMAIL=$6        
 else
     exit 1
 fi
 
+# Optional backup of mysql (need username and password)
+if [ -n "$7" -a -n "$8" ]; then
+    if [ -n "$HOST" -a -n "$SSH_COMMAND" ]; then
+        $SSH_COMMAND $HOST "mysqldump -u '$7' -p'$8' --all-databases | gzip > ~/mysql.gz"
+    else
+        mysqldump -u '$7' -p'$8' --all-databases | gzip > ~/mysql.gz
+    fi;
+fi
 
-# Do not touch unless sure about it
 
+# Standard variables
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
 DST="$BACKUP_REPO$BACKUP_NAME"
 EXCLUDE="$DIR/$BACKUP_NAME.exclude"
@@ -39,6 +67,7 @@ DATE=`date +%Y-%m-%d`   # Full date 2012-12-31
 DOW=`date +%w`          # Day of the week 1 is Monday
 DOM=`date +%d`          # Date of the Month e.g. 27
 
+# Create non-existing directories
 if [ ! -d "$DST/current" ]; then
     mkdir -p $DST/current
 fi
@@ -70,13 +99,15 @@ else
 	DATE_DST=$DST/Daily/`date +%A`
 fi
 
-
+# Execute the rsync task
 rsync -e "$SSH_COMMAND" --rsync-path="$RSYNC_PATH" -az --numeric-ids --stats --human-readable --delete --exclude-from "$EXCLUDE" --delete-excluded --link-dest=$DST/current $SRC $DST/incomplete > $LOG 2>&1 && cat $LOG | mail -s "Rsync $BACKUP_NAME: success" $EMAIL || cat $LOG | mail -s "Rsync $BACKUP_NAME: failed" $EMAIL
 
+# Delete existing destination directory
 if [ -d "$DATE_DST" ]; then
     rm -rf $DATE_DST
 fi
 
+# Move backup to destination and setup new reference directory 
 mv $DST/incomplete $DATE_DST
 rm -rf $DST/current 
 ln -s $DATE_DST $DST/current
